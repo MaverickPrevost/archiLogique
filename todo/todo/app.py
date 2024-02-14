3ee9340146211120c6e29b5455abc785fa8839eb
MaverickPrevost <maverickprevost@gmail.com>
2024-02-14 16:27:38 +0100

    Ajout des questionnaires, questions et réponses

--- todo/todo/app.py
+++ todo/todo/app.py
@@ -1,2 +1,14 @@
-from flask import Flask
-app = Flask( __name__ )
+# app.py
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+db.create_all()
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- todo/todo/models.py
+++ todo/todo/models.py
@@ -1,20 +1,62 @@
-tasks = [
-{
-    'id': 1,
-    'title ': 'Courses ',
-    'description ': 'Salade , Oignons , Pommes , Clementines ',
-    'done ': True
-},
-{
-    'id': 2,
-    'title ': 'Apprendre REST ',
-    'description ': 'Apprendre mon cours et comprendre les exemples ',
-    'done ': False
-},
-{
-    'id': 3,
-    'title ': 'Apprendre Ajax ',
-    'description ': 'Revoir les exemples et ecrire un client REST JS avec Ajax ',
-    'done ': False
-}
-]
+# models.py
+from datetime import datetime
+from app import db
+
+class Questionnaire(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    questions = db.relationship('Question', backref='questionnaire', lazy=True)
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return f"Questionnaire('{self.title}')"
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'questions': [q.to_json() for q in self.questions]
+        }
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    question_type = db.Column(db.String(20), nullable=False)  # 'simple' or 'multiple'
+    questionnaire_id = db.Column(db.Integer, db.ForeignKey('questionnaire.id'), nullable=False)
+    answers = db.relationship('Answer', backref='question', lazy=True)
+
+    def __init__(self, content, question_type, questionnaire_id):
+        self.content = content
+        self.question_type = question_type
+        self.questionnaire_id = questionnaire_id
+
+    def __repr__(self):
+        return f"Question('{self.content}', '{self.question_type}')"
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'question_type': self.question_type,
+            'answers': [a.to_json() for a in self.answers]
+        }
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+
+    def __init__(self, content, question_id):
+        self.content = content
+        self.question_id = question_id
+
+    def __repr__(self):
+        return f"Answer('{self.content}')"
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'content': self.content
+        }
